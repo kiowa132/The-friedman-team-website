7939c31940df05ecb14978abf2ba83242fd656ac
kiowa132 <kylejfriedman132@gmail.com>
2026-08-16 21:12:17 -0400

    v3

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -213,8 +213,8 @@ export const BlogPostPage: React.FC<BlogPostPageProps> = ({ onOpenConsultation, 
         <div className="prose prose-sm max-w-none pt-10
           [&>*+*]:mt-6
           [&_p]:text-[15px] [&_p]:sm:text-lg [&_p]:text-[#1C2B2E]/85 [&_p]:leading-[1.8] [&_p]:font-light
-          [&>p:first-of-type]:text-lg [&>p:first-of-type]:sm:text-xl [&>p:first-of-type]:text-[#1C2B2E] [&>p:first-of-type]:leading-[1.7]
-          [&>p:first-of-type::first-letter]:font-serif [&>p:first-of-type::first-letter]:text-[64px] [&>p:first-of-type::first-letter]:sm:text-[80px] [&>p:first-of-type::first-letter]:font-bold [&>p:first-of-type::first-letter]:text-[#0F5C63] [&>p:first-of-type::first-letter]:float-left [&>p:first-of-type::first-letter]:leading-[0.85] [&>p:first-of-type::first-letter]:mr-3 [&>p:first-of-type::first-letter]:mt-1
+          [&>div:first-child>p:first-of-type]:text-lg [&>div:first-child>p:first-of-type]:sm:text-xl [&>div:first-child>p:first-of-type]:text-[#1C2B2E] [&>div:first-child>p:first-of-type]:leading-[1.7]
+          [&>div:first-child>p:first-of-type::first-letter]:font-serif [&>div:first-child>p:first-of-type::first-letter]:text-[64px] [&>div:first-child>p:first-of-type::first-letter]:sm:text-[80px] [&>div:first-child>p:first-of-type::first-letter]:font-bold [&>div:first-child>p:first-of-type::first-letter]:text-[#0F5C63] [&>div:first-child>p:first-of-type::first-letter]:float-left [&>div:first-child>p:first-of-type::first-letter]:leading-[0.85] [&>div:first-child>p:first-of-type::first-letter]:mr-3 [&>div:first-child>p:first-of-type::first-letter]:mt-1
           [&_h2]:font-serif [&_h2]:text-2xl [&_h2]:sm:text-3xl [&_h2]:font-bold [&_h2]:text-[#0D2226] [&_h2]:mt-14 [&_h2]:pt-6 [&_h2]:border-t [&_h2]:border-[#C9A96A]/25
           [&_h3]:font-serif [&_h3]:text-xl [&_h3]:sm:text-2xl [&_h3]:font-bold [&_h3]:text-[#0D2226] [&_h3]:mt-10
           [&_a]:text-[#0F5C63] [&_a]:font-semibold [&_a]:underline [&_a]:decoration-[#C9A96A] [&_a]:decoration-2 [&_a]:underline-offset-2 [&_a]:transition-colors hover:[&_a]:text-[#C9A96A] hover:[&_a]:decoration-[#0F5C63]
